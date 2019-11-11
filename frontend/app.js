b49f7d88d23a8c26127896e2fc29ad046a6fe163
github username <github email address>
2019-11-11 15:46:46 -0700

    created style.css updated user show

--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,12 @@
-console.log('hello')
+
 fetch('http://127.0.0.1:3000/users')
     .then(handleResponse)
     .then(users => {
         users.map(user => {
+            console.log(user.id)
             const h1 = document.createElement('h1')
-
-            h1.innerText = user.name
+            h1.innerHTML = `<a href=user.html?id=${user.id}>${user.name}</a>`
+            // h1.innerText = user.name
 
             document.body.appendChild(h1)
         })
@@ -16,3 +17,6 @@ fetch('http://127.0.0.1:3000/users')
 function handleResponse(response){
     return response.json()
 }
+
+
+
