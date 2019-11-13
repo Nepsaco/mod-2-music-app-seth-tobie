9487f561a0e27899510c1ef8ff9776e4b5e11946
Nepsaco <tobie.tsuzuki@gmail.com>
2019-11-13 16:15:43 -0700

    update styling and functionality

--- backend/music_app/app/controllers/playlists_controller.rb
+++ backend/music_app/app/controllers/playlists_controller.rb
@@ -1,5 +1,7 @@
 class PlaylistsController < ApplicationController
 
+    before_action :find_playlist, only: [:update, :destroy]
+
     def index
         playlists = Playlist.all 
 
@@ -7,16 +9,29 @@ class PlaylistsController < ApplicationController
     end
 
     def create
-        newPlaylist = Playlist.create({
-            name: params[:name],
-            user_id: params[:user_id]
-        })
+      newPlaylist = Playlist.create({
+        name: params[:name],
+        user_id: params[:user_id]
+      })
         redirect_to "http://localhost:3001/userPlaylist.html?id=#{newPlaylist.user_id}"
     end
 
-    # private
-    #     def allowed_params
-    #         params.permit(:name)
-    #     end
+    def update
+      @playlist.update(allowed_params)
+    end
+
+    def destroy
+      @playlist.delete
+    end
+
+    private
+
+    def find_playlist
+      @playlist = Playlist.find(params[:id])
+    end
+
+    def allowed_params
+      params.require(:playlist).permit(:name, :user_id)
+    end
 
 end
--- frontend/userPlaylist.js
+++ frontend/userPlaylist.js
@@ -14,7 +14,7 @@ fetch(`http://localhost:3000/users/${id}`)
  
         header.append(h2, createButton)
         createPlaylistCards(user.playlists)
-        buttonClick()
+        createButtonClick()
     })
 
 function handleResponse(response){
@@ -24,18 +24,24 @@ return response.json()
 function createPlaylistCards (playlist) { 
   const playlistContainer = document.querySelector("#playlistContainer")
     playlist.map(playlist => {
+      const div = document.createElement('div')
       const h3 = document.createElement('h3')
-      console.log(playlist)
+
       h3.innerText = playlist.name
-      playlistContainer.appendChild(h3)
+      div.className = 'playlistCard'
+
+      playlistContainer.appendChild(div)
+      div.appendChild(h3)
+      updateButton(div, playlist)
     })
 
 }
 
-function buttonClick() {
+function createButtonClick() {
   
   const button = document.querySelector("#createButton")
   button.addEventListener("click", function (event) {
+    document.querySelector('#createButton').remove()
     
     const form = document.createElement("form")
     form.method = "POST"
@@ -47,6 +53,7 @@ function buttonClick() {
     
     const inputName = document.createElement('input')
     inputName.name = "name"
+    inputName.className = 'textInput'
     
     const inputId = document.createElement('input')
     inputId.type = "hidden"
@@ -55,6 +62,7 @@ function buttonClick() {
     
     const submitButton = document.createElement('input')
     submitButton.type = "submit"
+    submitButton.className = 'submitButton'
   
     console.log(id)
     
@@ -63,5 +71,57 @@ function buttonClick() {
     form.append(label, inputName, inputId, submitButton)
   })
 
+}
+
+function updateButton(div, playlist){
+    const button = document.createElement('button')
+
+    button.innerText = 'Rename Playlist'
+    button.className = 'updateButton'
+    
+    div.appendChild(button)
+    updateButtonClick(button, div, playlist)
+}
+
+function updateButtonClick(button, div, playlist) {
+  
+  button.addEventListener("click", function (event) {
+    const h3 = div.querySelector('h3')
+    h3.remove()
+    button.remove()
+    const form = document.createElement("form")
+
+    form.id = "updatePlaylistForm"
+    
+    const inputName = document.createElement('input')
+    inputName.name = "name"
+    inputName.type = 'text'
+    inputName.className = 'textInput'
+    inputName.value = playlist.name
+
+    const changeButton = document.createElement('input')
+    changeButton.type = "submit"
+    changeButton.value = 'Submit Changes'
+    changeButton.className = 'submitButton'
+
+    div.append(form)
+    form.append(inputName, changeButton)
+
+    changeButton.addEventListener('click', function(event){
+        event.preventDefault() 
+          fetch((`http://localhost:3000/playlists/${playlist.id}`), {
+            method: "PATCH",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                  playlist: {
+                      name: inputName.value
+                      // user_id: id
+                  }
+              })
+          }).then(r => window.location =`userPlaylist.html?id=${id}`)
+       })
+    })
 }
 
